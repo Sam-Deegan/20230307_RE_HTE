e578849c232b467ef769df4a757c9feb221eade7
Sam Deegan <sam.deegan@ucdconnect.ie>
2023-03-10 18:02:10 +0000

    Update Method Paper summary

--- 2_Analysis/B_Code/Analysis_Script.R
+++ 2_Analysis/B_Code/Analysis_Script.R
@@ -18,13 +18,13 @@ library(caret) # Machine Learning Package
 
 ## 2. Prepare Data ############################################################
 
-# Load data
+# Load Data
 data <- read.csv("your_data.csv")
 
-# Set treatment variable
+# Set Treatment Variable
 treatment <- "treatment_variable_name"
 
-# Set outcome variable
+# Set Outcome Variable
 outcome <- "outcome_variable_name"
 
 # Set covariates
@@ -32,29 +32,29 @@ covariates <- c("covariate1", "covariate2", "covariate3")
 
 ## 3. Split Data ##############################################################
 
-# Set seed for reproducibility
+# Set Seed for Reproducibility
 set.seed(123)
 
-# Split the data
+# Split the Data
 data_split <- trainTestSplit(data, testFraction = 0.3)
 train_data <- data_split$train
 test_data <- data_split$test
 
 ## 4. Fit Causal Forest Model #################################################
 
-# Fit the causal forest model
+# Fit the Causal Forest Model
 cf <- causal_forest(X = train_data[, covariates],
                     Y = train_data[, outcome],
                     W = train_data[, treatment])
 
 ## 6. Calculate Average Treatment Effects #####################################
 
-# Predict treatment effects
+# Predict Treatment Effects
 treatment_effects <- predict(cf, newdata = test_data[, covariates])
 
 ## 7. Evaluate Model ##########################################################
 
-# Calculate average treatment effect
+# Calculate Average Treatment Effect
 ate <- mean(treatment_effects)
 
 ## 8. Plots and Graphs ########################################################
